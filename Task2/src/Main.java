6d8fad7f033e5e3e0d8f4971ad8dee802aacd0a9
123456 <napnest@gmail.com>
2023-10-02 15:38:23 +0300

    Complete task 1 and task 2

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,33 @@
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
+        String alphabet = " -йцукеёнгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁ";
+        int probelCount=0;
+        boolean isValid=true;
+        String fio = new Scanner(System.in).nextLine();
+        String name = fio.substring(fio.indexOf(" "),fio.lastIndexOf(" ")).trim();
+        String surname = fio.substring(0,fio.indexOf(" ")).trim();
+        String otchestvo = fio.substring(fio.lastIndexOf(" ")).trim();
+
+        for (int i = 0; i <fio.length(); i++) {
+            if (fio.charAt(i) == ' ') {
+                probelCount++;
+            }
+            if (!alphabet.contains(String.valueOf(fio.charAt(i)))) {
+                isValid = false;
+                break;
+            }
+        }
+            if(isValid && probelCount==2){
+                System.out.println("Фамилия: "+surname);
+                System.out.println("Имя: "+name);
+                System.out.println("Отчество: "+otchestvo);
+            }
+            else{
+                System.out.println("Ввод неправильный");
+            }
+        }
+
     }
-}
